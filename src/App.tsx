bb6ba24c58c29c200872449a2c29465d1c9dd411
AndersonBillss <anderson@thebillsfamily.com>
2025-04-19 01:05:50 -0600

    Added styling for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className='main'>
       <HexagonBackground inverted className='hexagon-background'/>
-      <h1>Hello, World!</h1>
+      <div className='home-img-container'>
+        <div className='home-img-overlay'></div>
+        <img className='home-img' src='public/motherboard.jpg'></img>
+        <div className='home-img-title-container'>
+          <h1 className='home-img-title'>Hi, my name is Anderson</h1>
+        </div>
+      </div>
     </div>
   )
 }
