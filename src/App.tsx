8e78a684b0d1989ccc14984450ea0d1b1bc0cbde
AndersonBillss <anderson@thebillsfamily.com>
2025-04-23 00:17:38 -0600

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css'
 import Home from './pages/home/Home'
 import HexagonBackground from './shared/HexagonBackground'
 import MouseLight from './shared/MouseLight'
+import Navbar from './shared/navbar/Navbar'
 
 function App() {
 
   return (
     <div className='main'>
+      <Navbar />
       <HexagonBackground inverted className='hexagon-background'/>
-      <MouseLight radius={600} className='mouse-light'/>
+      <MouseLight radius={2000} className='mouse-light'/>
       <Home/>
     </div>
   )
---
+++ src/shared/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import "./navbar.css"
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+           <button className="button">Home</button>
+           <button className="button">See Projects</button>
+        </div>
+    )
+}
