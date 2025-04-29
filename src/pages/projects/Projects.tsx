03327986c2e7a33a4586af847dca532cd7d34832
AndersonBillss <anderson@thebillsfamily.com>
2025-04-28 19:06:58 -0600

    Added textInput dropdown options

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -7,7 +7,19 @@ export default function Projects(){
     return (
         <div className="projects-page">
             <h1>Projects</h1>
-            <TextInput label="Filter by technology used"/>
+            <TextInput 
+            label="Filter by technology"
+            dropDownOptions={[
+                "Dropdown Option 1",
+                "Dropdown Option 2",
+                "Dropdown Option 3",
+                "Dropdown Option 4",
+                "Dropdown Option 5",
+                "Dropdown Option 6",
+                "Dropdown Option 7",
+                "Dropdown Option 8",
+            ]}
+            />
             {projects.map((project: project, index: number) => <Project project={project} key={index} />)}
         </div>
     )
