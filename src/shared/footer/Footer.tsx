e6dfcb8a5c751f763b390d5c2ab4cc7f7f608467
AndersonBillss <anderson@thebillsfamily.com>
2025-09-18 19:32:08 -0600

    Added more info to footer

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -1,17 +1,39 @@
 import './footer.css'
 
-export default function Footer(){
+export default function Footer() {
     let iconColor: string | null = null
     const root: HTMLElement | null = document.getElementById('root');
-    if(root) iconColor = getComputedStyle(root).getPropertyValue('--text-light')
-    return(
+    if (root) iconColor = getComputedStyle(root).getPropertyValue('--text-light')
+    return (
         <div className='footer'>
             <a className='link' target='_blank' href='https://github.com/AndersonBillss'>
-            My Github 
-            <svg className='link-icon' height="16px" viewBox="0 -960 960 960" width="16px" fill={iconColor??"white"}>
-                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/>
-            </svg>
+                <svg height="32" aria-hidden="true" viewBox="0 0 24 24" version="1.1" width="32" data-view-component="true" fill="white">
+                    <path d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"></path>
+                </svg>
             </a>
+            <div style={{
+                display: "flex",
+                alignItems: "center"
+            }}>
+                <svg height="32px" viewBox="0 -960 960 960" width="32px" fill="white"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" /></svg>
+
+                <p style={{
+                    marginLeft: 5
+                }}>
+                    anderson@thebillsfamily.com
+                </p>
+            </div>
+            <div style={{
+                display: "flex",
+                alignItems: "center"
+            }}>
+                <svg height="24px" viewBox="0 -960 960 960" width="24px" fill="white"><path d="M162-120q-18 0-30-12t-12-30v-162q0-13 9-23.5t23-14.5l138-28q14-2 28.5 2.5T342-374l94 94q38-22 72-48.5t65-57.5q33-32 60.5-66.5T681-524l-97-98q-8-8-11-19t-1-27l26-140q2-13 13-22.5t25-9.5h162q18 0 30 12t12 30q0 125-54.5 247T631-329Q531-229 409-174.5T162-120Zm556-480q17-39 26-79t14-81h-88l-18 94 66 66ZM360-244l-66-66-94 20v88q41-3 81-14t79-28Zm358-356ZM360-244Z" /></svg>
+                <p style={{
+                    marginLeft: 5
+                }}>
+                    801-809-2505
+                </p>
+            </div>
         </div>
     )
 }
