44fe217429c056b7c96ab2f43bd92f364f0f7fba
AndersonBillss <anderson@thebillsfamily.com>
2025-09-18 19:34:09 -0600

    Removed text-input

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,25 +1,11 @@
 import "./projects.css"
 import { projects, project } from "../../projects"
 import Project from "./project/Project"
-import { TextInput } from "../../shared/textInput/TextInput"
 
 export default function Projects(){
     return (
         <div className="projects-page">
             <h1>Projects</h1>
-            <TextInput 
-            label="Filter by technology"
-            dropDownOptions={[
-                "Dropdown Option 1",
-                "Dropdown Option 2",
-                "Dropdown Option 3",
-                "Dropdown Option 4",
-                "Dropdown Option 5",
-                "Dropdown Option 6",
-                "Dropdown Option 7",
-                "Dropdown Option 8",
-            ]}
-            />
             {projects.map((project: project, index: number) => <Project project={project} key={index} />)}
         </div>
     )
